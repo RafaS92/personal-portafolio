ae41147375ce78f178553febd5df4bd06aa2744d
RAFAEL VALDEZ <manrvaldez.92@gmail.com>
2022-01-05 20:02:54 -0600

    minor fixes v1

--- src/components/v1/ResumeJumbotron.jsx
+++ src/components/v1/ResumeJumbotron.jsx
@@ -1,38 +1,31 @@
-import React, { Component } from "react";
-import { Jumbotron } from "react-bootstrap";
+import React, { Component } from 'react';
 
 class ResumeJumbotron extends Component {
   render() {
     return (
-      <div id="resume">
-        <Jumbotron className="Jumbotron-resume" fluid>
-          <div>
-            <div className="resume-container">
-              <div className="text-resume">
-                <h1>
-                  Check Out My LinkedIn!
-                  <p>Resume, education, interests and more...</p>
-                </h1>
-              </div>
-            </div>
-            <div className="resumeIcons-container">
-              <a href="https://www.linkedin.com/in/rafael-salvador-valdez/">
-                <img
-                  className="LikedIn-img"
-                  src="/images/LinkedIn.jpeg"
-                  alt=""
-                />
-              </a>
-              <a href="https://drive.google.com/file/d/13HfkPrkWcOqWfLh-gQ7UID_yDEk6T6JW/view?usp=sharing">
-                <img
-                  className="LikedIn-img"
-                  src="https://us.123rf.com/450wm/riduwanmolla/riduwanmolla1809/riduwanmolla180900031/110113326-resume-vector-icon-cv-icon-white.jpg?ver=6"
-                  alt=""
-                />
-              </a>
+      <div id='resume'>
+        <div className='resume-section'>
+          <div className='resume-container'>
+            <div className='text-resume'>
+              <h1>
+                Check Out My LinkedIn!
+                <p>Resume, education, interests and more...</p>
+              </h1>
             </div>
           </div>
-        </Jumbotron>
+          <div className='resumeIcons-container'>
+            <a href='https://www.linkedin.com/in/rafael-salvador-valdez/'>
+              <img className='LikedIn-img' src='/images/LinkedIn.jpeg' alt='' />
+            </a>
+            <a href='https://drive.google.com/file/d/13HfkPrkWcOqWfLh-gQ7UID_yDEk6T6JW/view?usp=sharing'>
+              <img
+                className='LikedIn-img'
+                src='https://us.123rf.com/450wm/riduwanmolla/riduwanmolla1809/riduwanmolla180900031/110113326-resume-vector-icon-cv-icon-white.jpg?ver=6'
+                alt=''
+              />
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
