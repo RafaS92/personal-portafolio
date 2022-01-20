e455146d41354b2513fce8e107217832a0347955
RAFAEL VALDEZ <manrvaldez.92@gmail.com>
2022-01-19 23:12:01 -0600

    dropdown functionallity working

--- src/components/v2/components/Skills.jsx
+++ src/components/v2/components/Skills.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as skillsData from '../../../data/skillsV2.json';
 import Dropdown from './shareComponents/Dropdown';
+import Test from './shareComponents/Test';
 // import { makeStyles, Typography } from '@material-ui/core';
 import './skills.css';
 // import zIndex from '@material-ui/core/styles/zIndex';
 
 function Skills() {
-  const [selected, setSelected] = useState('All');
+  const [selected, setSelected] = useState('Frontend');
 
-  const group = skillsData.skills.filter((e) => e.title === 'Backend');
+  let groupOfSkills = skillsData.skills.filter(
+    (e) => e.title === `${selected}`
+  );
+  let skillsIcons = groupOfSkills.map((e) => e.icons);
+  let arrayOfIcons = skillsIcons[0];
 
-  console.log('red');
-  console.log(skillsData.skills);
-  console.log(group);
   return (
     <section className='' id='skills-v2'>
       <div className='skills__container-v2'>
         <Dropdown selected={selected} setSelected={setSelected} />
         <h1>{selected}</h1>
+        {arrayOfIcons?.map((icon) => (
+          <img
+            style={{ width: '100px' }}
+            src={`/images/logos/${icon}`}
+            alt=''
+          />
+        ))}
       </div>
     </section>
   );
