0594edb0eb1d0144c2782009edad707a211bc610
RafaS92 <manrvaldez.92@gmail.com>
2023-11-26 11:34:06 -0500

    target blank added to links

--- src/components/v2/components/shareComponents/AnimatedButton.js
+++ src/components/v2/components/shareComponents/AnimatedButton.js
@@ -1,13 +1,13 @@
-import React, { useContext } from 'react';
-import AppContext from '../context/AppContext';
+import React, { useContext } from "react";
+import AppContext from "../context/AppContext";
 
 function OutlineButton(props) {
   const contextData = useContext(AppContext);
   let darkmode = contextData.darkmode.darkTheme;
   const { label, href } = props;
   return (
-    <div className={darkmode ? 'Outline-button' : 'Outline-button-white'}>
-      <a href={href}>
+    <div className={darkmode ? "Outline-button" : "Outline-button-white"}>
+      <a href={href} target="blank">
         <span>{label}</span>
       </a>
     </div>
