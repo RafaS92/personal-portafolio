96ce9f76567f284d7cc51562928a5c01a470d1b0
RafaS92 <manrvaldez.92@gmail.com>
2025-05-16 15:26:36 -0500

    test2

--- src/components/v2/components/Projects.js
+++ src/components/v2/components/Projects.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import projectsData from '../../../data/projectsData.json';
 import './Projects.css';
 import StatsContainer from './StatsContainer';
