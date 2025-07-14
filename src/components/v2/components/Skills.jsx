fe88e184ec5444b536eef81dc57aeca70bdee729
Rafa <rafa@Rafas-MacBook-Pro.local>
2025-07-14 14:24:18 +0200

    updating logos

--- src/components/v2/components/Skills.jsx
+++ src/components/v2/components/Skills.jsx
@@ -1,12 +1,13 @@
-import { useState, useContext, useEffect } from 'react';
-import skillsData from "../../../data/skillsV2.json";import Dropdown from './shareComponents/Dropdown';
-import './skills.css';
-import AppContext from './context/AppContext';
-import translate from '../i18n/translate';
+import { useState, useContext, useEffect } from "react";
+import skillsData from "../../../data/skillsV2.json";
+import Dropdown from "./shareComponents/Dropdown";
+import "./skills.css";
+import AppContext from "./context/AppContext";
+import translate from "../i18n/translate";
 
 function Skills() {
-  const [selected, setSelected] = useState('All');
-  const [addAnimation, setAddAnimation] = useState('');
+  const [selected, setSelected] = useState("All");
+  const [addAnimation, setAddAnimation] = useState("");
 
   const contextData = useContext(AppContext);
   let darkmode = contextData.darkmode.darkTheme;
@@ -18,9 +19,9 @@ function Skills() {
   let arraySkills = skillsIcons[0];
 
   function addAnimationIcons() {
-    setAddAnimation('animation');
+    setAddAnimation("animation");
     setTimeout(() => {
-      setAddAnimation('');
+      setAddAnimation("");
     }, 1500);
   }
 
@@ -29,19 +30,19 @@ function Skills() {
   }, [selected]);
 
   return (
-    <section id='Technologies-v2' className='skills-section'>
-      <h1 className='skills-title' data-aos='fade-right'>
-        {translate('title3')}
+    <section id="Technologies-v2" className="skills-section">
+      <h1 className="skills-title" data-aos="fade-right">
+        {translate("title3")}
       </h1>
       <Dropdown selected={selected} setSelected={setSelected} />
       <div
         className={
-          darkmode ? 'skills__container-v2' : 'skills__container-v2-white'
+          darkmode ? "skills__container-v2" : "skills__container-v2-white"
         }
       >
         {arraySkills?.map((icon) => (
           <div key={icon} className={`icon-img-container ${addAnimation}`}>
-            <img src={`/images/logos/${icon}`} alt='' className='icon-img ' />
+            <img src={`/images/logos/${icon}`} alt="" className="icon-img " />
           </div>
         ))}
       </div>
